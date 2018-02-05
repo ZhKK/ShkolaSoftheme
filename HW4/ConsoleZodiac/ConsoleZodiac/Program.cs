f2c5ec440b08cf60f729ee657d5e2efb08c88364
ZhKK <35741591+ZhKK@users.noreply.github.com>
2018-02-05 09:17:48 +0200

    4-5
    Add parts

---
+++ HW4/ConsoleZodiac/ConsoleZodiac/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleZodiac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For exit enter Esc.");
+            do
+            {
+                Console.WriteLine("Enter your birth date like DD/MM/YYYY:");
+                string []date = Console.ReadLine().Split('/');
+                Console.WriteLine(date);
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ HW5/Triangle/Triangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(" To draw a shape, enter the key: t - triangle, s - square: ");
+            char fig = char.Parse(Console.ReadLine());
+            
+            Console.WriteLine();
+            Console.WriteLine(" Enter a length of sides: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Press the Escape (Esc) key to quit: \n");
+        
+            /*private void ShowTriangle(object sender, RoutedEventArgs e)
+            {
+                
+                do
+                {
+                    int nextDigit = amount % 8;
+                    amount /= 8;
+                    int digitCode = '0' + nextDigit;
+                    char digit = Convert.ToChar(digitCode);
+                    current = digit + current;
+                    steps.Text += current + "\n";
+                }
+                while (amount != 0);
+            }*/
+        }
+    }
+}
