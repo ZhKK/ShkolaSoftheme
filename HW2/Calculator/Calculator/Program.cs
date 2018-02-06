0d44ed1f722e23158a47de7cff0d4321578e6f5e
ZhKK <35741591+ZhKK@users.noreply.github.com>
2018-02-07 01:36:22 +0200

    Re
    2-1, 2-2

--- HW2/Calculator/Calculator/Program.cs
+++ HW2/Calculator/Calculator/Program.cs
@@ -10,46 +10,95 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-                Console.WriteLine("Press Esc to exit");
+            Console.WriteLine("Press Esc to exit.");
             do
             {
                 try
                 {
-                    Console.WriteLine("Enter 1st number:");
-                    double a = double.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Enter an operator:");
-                    char oprt = char.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Enter 2nd number:");
-                    double b = double.Parse(Console.ReadLine());
-                    Console.WriteLine("_______________");
-                    Console.WriteLine("Result:");
+                    double a, b;
+                    char oprt;
+                    MethodInPut(out a, out oprt, out b);
 
                     double rez = 0;
-                    if (oprt == '+')
-                    {
-                        rez = a + b;
-                        Console.WriteLine(a + " + " + b + " = " + "{0:##.##}", rez);
-                    }
-                
-                    if (oprt == '-')
-                        Console.WriteLine(a + " - " + b + " = " + "{0:##.##}", (a - b));
-                    if (oprt == '*')
-                        Console.WriteLine(a + " * " + b + " = " + "{0:##.##}", (a * b));
-                    if (oprt == '/')
-                    {
-                        rez = a / b;
-                        Console.WriteLine(a + " / " + b + " = " + "{0:##.##}", rez);
-                    }
+                    MethodSum(a, oprt, b, rez);
+                    MethodMinus(a, oprt, b);
+                    MethodMultipl(a, oprt, b);
+
+                    rez = MethodDivid(a, oprt, b, rez);
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Enter less numbers!");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Use the correct input formats!");
+                }
+                finally
+                {
                     Console.WriteLine();
                 }
-                    catch (DivideByZeroException) {
-                        Console.WriteLine("Сan not be divided by zero!");
-                    }
             }
             while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
 
+        private static void MethodInPut(out double a, out char oprt, out double b)
+        {
+            Console.WriteLine("Enter 1st number:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter an operator:");
+            oprt = char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number:");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("_______________");
+            Console.WriteLine("Result:");
+        }
+
+        private static double MethodDivid(double a, char oprt, double b, double rez)
+        {
+            if (oprt == '/')
+            {
+                try
+                {
+                    rez = a / b;
+                    Console.WriteLine(a + " / " + b + " = " + "{0:##.##}", rez);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Сan not be divided by zero!");
+                }
+                finally
+                {
+                    Console.WriteLine("Сan not be divided by zero!");
+                }
+            }
+
+            return rez;
+        }
+
+        private static void MethodMultipl(double a, char oprt, double b)
+        {
+            if (oprt == '*')
+            {
+                Console.WriteLine(a + " * " + b + " = " + "{0:##.##}", (a * b));
+            }
+        }
+
+        private static void MethodMinus(double a, char oprt, double b)
+        {
+            if (oprt == '-')
+            {
+                Console.WriteLine(a + " - " + b + " = " + "{0:##.##}", (a - b));
+            }
+        }
+
+        private static void MethodSum(double a, char oprt, double b, double rez)
+        {
+            if (oprt == '+')
+            {
+                rez = a + b;
+                Console.WriteLine(a + " + " + b + " = " + "{0:##.##}", rez);
+            }
         }
     }
 }
--- HW2/MinMax/MinMax/MainWindow.xaml.cs
+++ HW2/MinMax/MinMax/MainWindow.xaml.cs
@@ -32,6 +32,9 @@ namespace MinMax
 
             switch (selectedItem.Content.ToString())
             {
+                default:
+                    ShowStringValue();
+                    break;
                 case "sbyte":
                     ShowStringValue();
                     break;
@@ -63,64 +66,80 @@ namespace MinMax
         {
             int intMin = int.MinValue;
             int intMax = int.MaxValue;
+            int intDefault = default(int);
             valueMin.Text = intMin.ToString(CultureInfo.InvariantCulture);
             valueMax.Text = intMax.ToString(CultureInfo.InvariantCulture);
+            valueDefault.Text = intDefault.ToString(CultureInfo.InvariantCulture);
         }
 
         private void ShowLongValue()
         {
             long longMin = long.MinValue;
             long longMax = long.MaxValue;
+            long longDefault = default(long);
             valueMin.Text = longMin.ToString(CultureInfo.InvariantCulture);
             valueMax.Text = longMax.ToString(CultureInfo.InvariantCulture);
+            valueDefault.Text = longDefault.ToString(CultureInfo.InvariantCulture);
         }
 
         private void ShowFloatValue()
         {
             float floatMin = float.MinValue;
             float floatMax = float.MaxValue;
+            float floatDefault = default(float);
             valueMin.Text = floatMin.ToString(CultureInfo.InvariantCulture);
             valueMax.Text = floatMax.ToString(CultureInfo.InvariantCulture);
+            valueDefault.Text = floatDefault.ToString(CultureInfo.InvariantCulture);
         }
 
         private void ShowDoubleValue()
         {
-            var doubleMin = double.MinValue;
-            var doubleMax = double.MaxValue;
+            double doubleMin = double.MinValue;
+            double doubleMax = double.MaxValue;
+            double doubleDefault = default(double);
             valueMin.Text = doubleMin.ToString(CultureInfo.InvariantCulture);
             valueMax.Text = doubleMax.ToString(CultureInfo.InvariantCulture);
+            valueDefault.Text = doubleDefault.ToString(CultureInfo.InvariantCulture);
         }
 
         private void ShowDecimalValue()
         {
             decimal decimalMin = decimal.MinValue;
             decimal decimalMax = decimal.MaxValue;
+            decimal decimalDefault = default(decimal);
             valueMin.Text = decimalMin.ToString(CultureInfo.InvariantCulture);
             valueMax.Text = decimalMax.ToString(CultureInfo.InvariantCulture);
+            valueDefault.Text = decimalDefault.ToString(CultureInfo.InvariantCulture);
         }
         
         private void ShowStringValue()
         {
             sbyte sbyteMin = sbyte.MinValue;
             sbyte sbyteMax = sbyte.MaxValue;
+            sbyte sbyteDefault = default(sbyte);
             valueMin.Text = sbyteMin.ToString(CultureInfo.InvariantCulture);
             valueMax.Text = sbyteMax.ToString(CultureInfo.InvariantCulture);
+            valueDefault.Text = sbyteDefault.ToString(CultureInfo.InvariantCulture);
         }
 
         private void ShowCharValue()
         {
             byte byteMin = byte.MinValue;
             byte byteMax = byte.MaxValue;
+            byte byteDefault = default(byte);
             valueMin.Text = byteMin.ToString(CultureInfo.InvariantCulture);
             valueMax.Text = byteMax.ToString(CultureInfo.InvariantCulture);
+            valueDefault.Text = byteDefault.ToString(CultureInfo.InvariantCulture);
         }
 
         private void ShowBoolValue()
         {
             short shortMin = short.MinValue;
             short shortMax = short.MaxValue;
+            short shortDefault = default(short);
             valueMin.Text = shortMin.ToString(CultureInfo.InvariantCulture);
             valueMax.Text = shortMax.ToString(CultureInfo.InvariantCulture);
+            valueDefault.Text = shortDefault.ToString(CultureInfo.InvariantCulture);
         }
 
         private void QuitClick(object sender, RoutedEventArgs e)
