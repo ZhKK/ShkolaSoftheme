9d641e5afdd7fcb67bc9df2f47571000ec6f5b71
ZhKK <35741591+ZhKK@users.noreply.github.com>
2018-02-24 17:53:52 +0200

    Add Methods and Interface
    Methods and Interface

---
+++ HW12/Interface/Interface/IUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IUser
+    {
+        string Name { get; }
+        string Password { get; }
+        string Email { get; }
+
+        string GetFullInfo();
+    }
+}
---
+++ HW12/Interface/Interface/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class User : IUser
+    {
+        
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Password { get; private set; }
+
+        public string GetFullInfo()
+        {
+            var info = string.Format("Name: {0}, \nEmail: {1}, \nPassword: {2}", Name, Email, Password);
+            return info;
+        }
+    }
+}
